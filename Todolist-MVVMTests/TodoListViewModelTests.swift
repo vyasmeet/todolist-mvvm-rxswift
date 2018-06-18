e6eb5c8c311f6b0dababb5233ec78d61c76ce3cd
Meet Vyas <meet.vyas@asticsinc.com>
2018-06-18 17:01:46 +0530

    Realm test cases added.

--- Todolist-MVVMTests/TodoListViewModelTests.swift
+++ Todolist-MVVMTests/TodoListViewModelTests.swift
@@ -12,12 +12,14 @@ import XCTest
 class TodoListViewModelTests: XCTestCase {
     
     var vm: TodoListViewModel!
+    var vmRealm: TodoListRealmViewModel!
     
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
         
         vm = TodoListViewModel()
+        vmRealm = TodoListRealmViewModel()
     }
     
     override func tearDown() {
@@ -25,6 +27,7 @@ class TodoListViewModelTests: XCTestCase {
         super.tearDown()
         
         vm = nil
+        vmRealm = nil
     }
     
     func testAddTwoTodos() {
@@ -37,6 +40,16 @@ class TodoListViewModelTests: XCTestCase {
         XCTAssert(vm.getTodos().value.count == oldCount + 2)
     }
     
+    func testRealmAddTwoTodos() {
+        let oldCount = vmRealm.getTodos().value.count
+        
+        vmRealm.addTodo(withTodo: "newTest")
+        vmRealm.addTodo(withTodo: "newTest789")
+        
+        print("TODOS ==> ", vmRealm.getTodos().value)
+        XCTAssert(vmRealm.getTodos().value.count == oldCount + 2)
+    }
+    
     func testRemoveTodo() {
         let oldCount = vm.getTodos().value.count
         
@@ -45,14 +58,34 @@ class TodoListViewModelTests: XCTestCase {
         XCTAssert(vm.getTodos().value.count == oldCount - 1)
     }
     
+    func testRealmRemoveTodo() {
+        let oldCount = vmRealm.getTodos().value.count
+        
+        vmRealm.removeTodo(withIndex: 0)
+        
+        XCTAssert(vmRealm.getTodos().value.count == oldCount - 1)
+    }
+    
     func testToggleTodo() {
         let isCompleted = vm.getTodos().value[0].isCompleted
         vm.toggleTodoIsCompleted(withIndex: 0)
         XCTAssert(vm.getTodos().value[0].isCompleted == !isCompleted)
     }
     
+    func testRealmTogggleTodo() {
+        let isCompleted = vmRealm.getTodos().value[0].isCompleted
+        
+        vmRealm.toggleTodoIsCompleted(withIndex: 0)
+        
+        XCTAssert(vmRealm.getTodos().value[0].isCompleted == !isCompleted)
+    }
+    
     func testPrintTodos() {
         print("TODOS : ", vm.getTodos().value)
+    }
+    
+    func testRealmPrintTodos() {
+        print("TODOS ==> ", vmRealm.getTodos().value)
     }
     
     func testPerformanceExample() {
